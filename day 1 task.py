ee3c06ee4a718fef9a764da9cf0a12fdc7f8e980
sharubency <85781613+sharubency@users.noreply.github.com>
2021-06-13 19:43:04 +0530

    Add files via upload

---
+++ day 1 task.py
@@ -0,0 +1,15 @@
+# addition
+def add(a,b):
+    ans = a+b
+    return ans
+
+addition = add(5,6)
+print("Addition of 5 and 6 is {0}".format(addition))
+
+# maximum number
+
+def maximum(a,b,c,d):
+    result = max(a,b,c,d)
+    return result
+max_num = maximum(2,4,6,8)
+print("maximum number of 2 4 6 8 is {0}".format(max_num))
---
+++ day 2 task.py
@@ -0,0 +1,51 @@
+# printing value
+print("30 days 30 hours challenge")
+
+# assigning string to varable
+Hours = "Thirty"
+print(Hours)
+
+# indexing
+days = "thirty days"
+print(days[2])
+
+# print particular character
+challenge = "I will win"
+print(challenge[2:6])
+
+# lenght of character
+print(len(challenge))
+
+# lowercase
+print(challenge.lower())
+
+
+# string concatenation and space 
+
+a= "30 days"
+b= " 30 hours"
+print(a+b)
+
+
+# casefold
+
+text="Thirty days Thirty hours"
+x= text.casefold()
+print(x)
+
+# captalize,find,isalpha,isalnum
+
+text="Thirty days Thirty hours"
+
+a = text.capitalize()
+b = text.find('d')
+c = text.isalpha()
+
+
+print(a)
+print(b)
+print(c)
+
+alpha = "Thirty days 30 hours"
+s = alpha.isalnum()
+print(s)
