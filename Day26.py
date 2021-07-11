7e9fdfc5b15e0ba8b303e76f67847d73b283181a
sharubency <85781613+sharubency@users.noreply.github.com>
2021-07-11 20:28:00 +0530

    Add files via upload

---
+++ Day26.py
@@ -0,0 +1,10 @@
+#•	Enumerate a python list and try to print the counter with the list value
+list1=["sharpner","pencil","eraser","pen","book"]
+object1=enumerate(list1)
+print(list(object1))
+
+
+#•	Enumerate a python tuple and try to print the counter with the tuple value
+tuple1=("sharpner","pencil","eraser","pen","book")
+object2=enumerate(tuple1)
+print(tuple(object2))
